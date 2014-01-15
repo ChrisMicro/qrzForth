83bfdad94dd94e474363476072b17133f091ba5b
ChrisMicro <christoph@roboterclub-freiburg.de>
2014-01-15 15:53:24 +0100

    first version of bootloader

--- forth_src/startup.fs
+++ forth_src/startup.fs
@@ -18,6 +18,12 @@
 
 \ Comparisons
 : < 		swap > ;
+: >=    < invert ;
+
+\ Arithmetic
+: negate    invert 1+ ;
+: -         negate + ;
+: 2* _shl ;
 
 : rshift ( x1 u -- x2 ) 0do _asr loop ; \ shifts bits of x1 u places to the left, putting zeros in the empty places on the right.
 : lshift ( x1 u -- x2 ) 0do _shl loop ; \ shifts bits of x1 u places to the right, putting zeroes in the into empty places on the left.
@@ -44,6 +50,7 @@
 : 2/ ( n -- n/2 ) _asr ;
 
 : c@  ( c-adr -- n )  dup 2/ @ swap d# 1 and if d# 8 rshift else d# 255 and then ;
+
 : type ( adr n -- )
     d# 0do
         dup c@ emit
@@ -76,8 +83,113 @@
 
 : .		( n -- ) 1 external ; \ call external c function to print number on console
 
-\ set reset vector to test3
-main test3
+
+\ convert character to hex value
+: digit? ( c -- n f ) 
+	[char] 0 - 
+	dup d# 9 > 							
+	if											\ if no digit, it could be A,B ... 
+		7 - 
+		dup h# f > 
+			if d# 32 - then     \ if not A,B .. it could be a,b, ..			
+	then
+	dup h# fff0 and 0= 			\ check if some upper bits are set
+;
+
+: keydig
+	key digit? ( n f )
+;
+
+: num
+	0
+	begin
+		keydig while
+			swap 4 lshift
+			or
+		repeat
+;
+\ check if the input character is a digit and get the other keys
+\ or return the character and false
+: number? ( c -- n/c t/f )
+	dup
+	digit?
+	if
+		begin
+		keydig while
+			swap 4 lshift
+			or
+		repeat
+		0<> rot drop
+	else
+	  drop
+		0 								\ return false
+	then
+;		 
+
+
+: space h# 20 emit ;
+
+\ dump words
+: dump16 ( addr c - adr c)
+    swap
+	    dup 2* hex4 [char] : emit space 		\ show address
+	    
+	    d# 8 																\ 8 columns
+	    
+	    0do
+	        dup @ hex4 space
+	        1+
+	    loop
+	    cr
+    swap
+;
+
+\ show help
+: help 	cr
+					s" h: help" 			type cr
+					s" y: dump" 			type cr
+					s" g:go   " 			type cr
+					s" r: adr=start" 	type cr
+				cr
+;
+
+: startadr h# 800 ;
+
+: resetadr ( adr c -- 0 c )
+	s" reset address" type cr
+	swap drop startadr swap
+;
+: runcode startadr 2* >r ;
+	 				
+: bootloader 	
+					s" bootloader" type cr 
+					help																		\ show help
+					startadr 																\ set start adr
+
+					begin -1 while 													\ endless loop
+					
+							key 			( adr c ) 
+							number? 	( adr n/c t/f )
+							if 				( adr n ) 						
+	
+									over ! 															\ store value in address
+									1+ 																	\ increment word address
+							
+							else 			( adr c )
+							
+									dup 	[char] y = if dump16	 	then
+									dup 	[char] r = if resetadr 	then
+									dup 	[char] h = if help 			then		
+									dup 	[char] g = if runcode		then				
+									drop
+							
+							then
+
+					repeat 
+;				
+				
+\ set reset vector to bootloader
+main bootloader
 
 \ save code
 save
